f9660f19e6851de08fd566790ca7a05fcc24f089
robert <robertsantos98@hotmail.co.uk>
2019-11-18 17:06:39 +0000

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const server = require("./simplehttp.js");
+const server = require("./simpleHTTP.js");
 server.start({"port":"80","docroot":"./htdocs","status":"auto"});
 server.get( "/", ()=>{
     server.send( "<h1>HELLO</h1>" );
